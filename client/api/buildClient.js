dcb75e1973f32dda9a47007b574570e1d4a54ea2
Vladimir Sterlin <vovasterlin@yahoo.com>
2020-08-19 15:21:33 -0700

    fixes

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -4,7 +4,7 @@ const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
     return axios.create({
       baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+        "http://www.vspages.xyz",
       headers: req.headers,
     });
   } else {
